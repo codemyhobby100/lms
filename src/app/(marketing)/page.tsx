fa3dfb17a4213948317e8459f1dcc261b593cff1
Codemyhobby <alhassandamilola16@gmail.com>
2024-08-08 09:45:08 +0100

    features section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -11,6 +11,7 @@ import { cn } from "@/lib/utils";
 import { ArrowRight, ChevronRight, UserIcon, Zap } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { CardHoverEffectDemo } from "../snippet/card-hover-effect-snippet";
 
 const HomePage = () => {
 
@@ -135,7 +136,7 @@ const HomePage = () => {
                 </Container>
                 <Container>
                     <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
-                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-8">
+                        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-8">
                             {features.map((feature) => (
                                 <div key={feature.title} className="flex flex-col items-start lg:items-start px-0 md:px-0">
                                     <div className="flex items-center justify-center">
@@ -149,7 +150,8 @@ const HomePage = () => {
                                     </p>
                                 </div>
                             ))}
-                        </div>
+                        </div> */}
+                        <CardHoverEffectDemo/>
                     </div>
                 </Container>
             </Wrapper>
---
+++ src/app/snippet/card-hover-effect-snippet.tsx
@@ -0,0 +1,75 @@
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { PiAppStoreLogo, PiHeadsetFill, PiLock, PiMegaphone, PiMonitor, PiStorefront, PiChalkboardTeacherLight } from "react-icons/pi";
+import { FaRegMoneyBillAlt } from "react-icons/fa";
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
+
+export function CardHoverEffectDemo() {
+  return (
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
+    </div>
+  );
+}
+
+export const projects = [
+  {
+    icon: (
+      <div className="p-4 rounded-full border-2 text-[20px]">
+        <PiStorefront/>
+      </div>
+    ),
+    title: "Shopify Store Setup",
+    description:
+      "You will learn in detailed Videos how to setup your store from scratch to finish.",
+  },
+  {
+    icon: (
+      <div className="  p-4 rounded-full border-2 text-[20px]">
+        <FaRegMoneyBillAlt/>
+      </div>
+    ),
+    title: "Integration & Payment System",
+    description:
+      "You will Learn how to Integrate Payment Apps into your store and automate Products Fulfilment.",
+  },
+  {
+    icon: (
+      <div className="border-2 text-[20px]  p-4 rounded-full">
+        <PiMegaphone/>
+      </div>
+    ),
+    title: "Ads Setup & Marketing",
+    description:
+      "You will Learn how to Promote your Store and Make sales through Ads & marketing channels such as Facebook & Tiktok",
+  },
+  {
+    icon: (
+      <div className="border-2 text-[20px] p-4 rounded-full">
+        <MdOutlineAdminPanelSettings />
+      </div>
+    ),
+    title: "Business Administration",
+    description:
+      "Receive guidance from experienced Shopify mentors with personalized feedback and one-on-one sessions.",
+  },
+  {
+    icon: (
+      <div className="border-2 text-[20px] p-4 rounded-full">
+        <PiChalkboardTeacherLight />
+      </div>
+    ),
+    title: "Mentorship",
+    description:
+      "Learn essential management techniques, financial planning, and strategic decision-making for your store.",
+  },
+  {
+    icon: (
+      <div className="border-2 text-[20px] p-4 rounded-full">
+        <PiHeadsetFill />
+      </div>
+    ),
+    title: "Support",
+    description:
+      "Access live Q&A sessions, a community forum, and 24/7 assistance to help you stay on track.",
+  },
+];
