6145dd25a14a0e755c641c553d911c4870491452
Codemyhobby <alhassandamilola16@gmail.com>
2024-08-09 20:45:09 +0100

    discover

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -129,11 +129,11 @@ const HomePage = () => {
                         </p>
                     </div>
                 </Container>
-                <Container>
+                {/* <Container>
                     <div className="flex items-center justify-center mx-auto mt-8">
                         <Icons.feature className="w-auto h-80" />
                     </div>
-                </Container>
+                </Container> */}
                 <Container>
                     <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
                         {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-8">
